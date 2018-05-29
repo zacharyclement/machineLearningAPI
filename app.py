ad8be2703da5692a361f25cb37008eb4bdf600ec
zacharyclement <zachclem42@gmail.com>
2018-05-29 09:50:28 -0400

    working string data transfer from front end

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/string', methods=['GET','POST'])
-def get_post_string():
+@app.route('/form', methods=['GET','POST'])
+def form():
     if request.method == 'POST':
-        app_data = request.form
-        print('string data, ', app_data)
-        return jsonify(app_data)
+        form_data = request.form
+        print('form data, ', form_data)
+        return jsonify(form_data)
     else:
         dic = {"language": "python",
 	            "framework": "Flask",
@@ -28,12 +28,12 @@ def get_post_string():
         return jsonify(dic)
 
 @app.route('/file', methods=['GET','POST'])
-def get_post_file():	
+def file_data():	
 	if request.method == 'POST':
-		file_data = request.form
+		file_data = request.file
 		print('string data, ', file_data)
 		print(type(file_data))
-		return jsonify('hello')
+		return jsonify('hello files')
 	else:
 		dic = {"language": "python",
 	            "framework": "Flask",
@@ -47,14 +47,19 @@ def get_post_file():
 	
 	            }
 		return jsonify(dic)
-@app.route('/json', methods=['GET','POST'])
-def pos_post_string():
-    if request.method == 'POST':
-        json_data = request.get_json()
-        print('json data, ', json_data)
-        return jsonify(json_data)
-    else:
-        dic = {"language": "python",
+
+@app.route('/json', methods=['GET','POST','OPTIONS'])
+def json_data():
+	if request.method == 'POST':
+		print('request is POST')
+		test_json_data = request.get_json()
+		print('test_json_data: ', test_json_data)
+		d = json.loads(test_json_data)
+		print('json data: ', d)
+		return 'No Data Received'
+	
+	else:
+		dic = {"language": "python",
 	            "framework": "Flask",
 	            "name": "Zach",
 	            "v": {
@@ -65,7 +70,30 @@ def pos_post_string():
 	            "boolean_test" : 'true',
 	
 	            }
-        return jsonify(dic)
+		return jsonify(dic)
+
+@app.route('/data', methods=['GET','POST'])
+def data():
+	if request.method == 'POST':
+		data_data = request.data
+		print('data data, ', data_data)
+		d = json.loads(data_data)
+		print('data_data dictionary', d)
+		print('type: ', type(d))
+		return jsonify(d)
+	else:
+		dic = {"language": "python",
+	            "framework": "Flask",
+	            "name": "Zach",
+	            "v": {
+		                "p" : 3.4,
+		                "f" : 12
+	             },
+	            "examples" : ["hi", "hello"],
+	            "boolean_test" : 'true',
+	
+	            }
+		return jsonify(dic)
         
 
 if __name__ == '__main__':
